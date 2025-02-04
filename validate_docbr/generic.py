56285f4db5420f77aa6f4d16f6bd334fc3d8b725
Álvaro Paiva <alvarofepipa@gmail.com>
2025-02-04 20:24:50 -0300

    chore: Renomeia BaseDoc para DocumentBase

--- validate_docbr/generic.py
+++ validate_docbr/generic.py
@@ -1,15 +1,15 @@
 import inspect
 from typing import List, Tuple, Type
 
-from .BaseDoc import BaseDoc
+from .DocumentBase import DocumentBase
 
 
-def validate_docs(documents: List[Tuple[Type[BaseDoc], str]] = list) -> list[bool]:
+def validate_docs(documents: List[Tuple[Type[DocumentBase], str]] = list) -> list[bool]:
     """Recebe uma lista de tuplas (classe, valor) e a valida"""
     validations = []
 
     for doc in documents:
-        if not inspect.isclass(doc[0]) or not issubclass(doc[0], BaseDoc):
+        if not inspect.isclass(doc[0]) or not issubclass(doc[0], DocumentBase):
             raise TypeError(
                 "O primeiro índice da tupla deve ser uma classe de documento!"
             )
